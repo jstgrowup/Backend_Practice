42d2b8cca471ddb7afa034af9e249d6d3eecfbde
jstgrowup <deysubham999@gmail.com>
2023-11-24 21:20:04 +0530

    fixed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 // require("dotenv").config();
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
+const port = process.env.PORT || 3000;
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server connected ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongoDB error", err);
+  });
---
+++ src/utils/error.js
@@ -0,0 +1,16 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.sucess = false;
+    this.error = errors;
+  }
+}
+export {ApiError}
